cdbc45a3283df05930dc6883b4f62fddede821d2
ProgErnest <cuba7843@gmail.com>
2026-07-07 13:06:31 +0200

    update the process of oop paradigms

--- paradigms/oop_cards.py
+++ paradigms/oop_cards.py
@@ -1,33 +1,45 @@
 
 class Product:
+
     def __init__(self, name, unit_price, quantity):
-        self.name = name
-        self.unit_price = unit_price
-        self.quantity = quantity
+        self.__name = name
+        self.__unit_price = unit_price
+        self.__quantity = quantity
+
     def get_total_price(self):
-        return self.unit_price * self.quantity
+        return self.__unit_price * self.__quantity
+    
     def display_product_info(self):
-        print(f"Name: {self.name}\nUnit price: {self.unit_price}\nQuantity: {self.quantity}")
+        print(f"Name: {self.__name}\nUnit price: {self.__unit_price}\nQuantity: {self.__quantity}")
+
+
 class Cart:
     def __init__(self):
-        self.products = []
-        self.total_price = 0
+        self.__products = []
+        self.__total_price = 0
+
     def add_product(self, product):
-        self.products.append(product)
+        self.__products.append(product)
+
     def get_total_price(self):
-        for product in self.products:
-            self.total_price += product.get_total_price()
-        return self.total_price
+        self.__total_price = 0
+        for product in self.__products:
+            self.__total_price += product.get_total_price()
+        return self.__total_price
+    
     def apply_discount(self):
-        if self.total_price > 50000:
-            self.total_price *= 0.9
-    def display_informations(self):
-        for product in self.products:
-            product.display_product_info()
-        print(f"\n\n The total price is {self.total_price}")
+        if self.__total_price > 50000:
+            self.__total_price = self.__total_price * 0.9
+            print("A discount has been applied")
+            return self.__total_price
+        else:
+            print("No discount")
+            return self.__total_price
+
 
 if __name__ == "__main__":
     shopping_cart = Cart()
+
     while True:
         product_name = input("Enter the name of the product: ")
         product_unit_price = float(input("\nEnter the unit price of the product: "))
@@ -35,13 +47,12 @@ if __name__ == "__main__":
         product = Product(product_name, product_unit_price, product_quantity)
         continue_shopping = input("\nDo you want to add another product? (yes/no): ")
         shopping_cart.add_product(product)
+
         if(continue_shopping == "yes"):
             continue
         break
 
-total_price = shopping_cart.get_total_price()
-discounted_price = shopping_cart.apply_discount()
-new_total_price = shopping_cart.get_total_price()
-result= f"Your total cart is :{total_price}" if(total_price == new_total_price) else f"You have a dicscount your new price is {new_total_price}"
-print(result)
-shopping_cart.display_informations()
+    total_price = shopping_cart.get_total_price()
+    discounted_price = shopping_cart.apply_discount()
+    result= f"Your total cart is :{discounted_price:_.2f} FCFA\n thank you for your confidence".replace('_',' ')
+    print(result)
